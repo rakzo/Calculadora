056fc61588b84a0f0ebae42b0a8b5079e19d4475
rakzo <oscarvelandia02@gmail.com>
2014-11-27 16:00:03 -0500

    1era parte codigo Rakzo

--- Calculadora/src/main/Main.java
+++ Calculadora/src/main/Main.java
@@ -11,5 +11,32 @@ public class Main {
 		System.out.println("1er cambio en rama master");
 		System.out.println("2do cambio en rama master");
 	}
+	public class Operaciones extends javax.swing.JFrame {
+
+		public Operaciones() {
+		initComponents();
+		    }
+		    
+		String numero1 = "";//creo una variable string, van a contener un espacio, donde no tienen nada
+		String numero2 = "";
+		Int contador = 0;//
+
+
+		 private void resultadoActionPerformed(java.awt.event.ActionEvent evt) {                                          
+		        if(etiqueta1.getText().equals("")|| contador != 0){
+		           operaciones();
+		           etiqueta2.setText("");
+		        }
+		    }                                         
+
+		    private void borradorActionPerformed(java.awt.event.ActionEvent evt) {                                         
+		        numero1 = "";
+		        numero2 = "";
+		        etiqueta1.setText("");
+		        etiqueta2.setText("");
+		        etiqueta3.setText("");//muestre espacio
+		        contador = 0;//vuelve el contador a reinicarse
+		    } 
+
 
 }
