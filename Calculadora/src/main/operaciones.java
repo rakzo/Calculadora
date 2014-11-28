0e7bb2daf199e99aa5552dfd129d3c422048c26e
marysalazar <marysalazar@unicauca.edu.co>
2014-11-27 21:31:48 -0500

    Update operaciones.java

--- Calculadora/src/main/operaciones.java
+++ Calculadora/src/main/operaciones.java
@@ -1,5 +1,11 @@
 package main;
 
 public class operaciones {
+  private void porActionPerformed(java.awt.event.ActionEvent evt) {                                    
+        if(etiqueta3.getText().equals("")|| contador != 0){
+           operaciones();
+           etiqueta2.setText("*");
+        }
+    }                                   
 
 }
